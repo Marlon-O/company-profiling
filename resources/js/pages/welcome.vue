58c65b8be21c9abc383df93c4ff63be96f9eee22
Marlon Ocsan <marlonocsan26@gmail.com>
2023-05-24 22:40:23 +0800

    Initial commit

---
+++ resources/js/pages/welcome.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="text-center">
+      <div class="text-6xl font-thin text-gray-500 mt-16 mb-8" dusk="title">
+        {{ title }}
+      </div>
+
+      <div>
+        <p class="font-bold">by Marlon Ocsan</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  layout: 'default',
+
+  metaInfo () {
+    return { title: this.$t('home') }
+  },
+
+  data: () => ({
+    title: window.config.appName
+  }),
+
+  computed: mapGetters({
+    authenticated: 'auth/check'
+  })
+}
+</script>
+
+<style scoped>
+.top-right {
+  position: absolute;
+  right: 10px;
+  top: 18px;
+}
+
+</style>
